0757e6e6ec646a1040bd6bfc936028765d601e1a
Henock Belachew <HenockBelachew3436@gmail.com>
2023-03-15 09:38:35 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function changeImArb(){
+   let attraction1 = document.getElementById('image'); 
+   attraction1.src = 'image/Arbaminch1.jpg';
+   document.getElementById('TourCity').innerHTML='Arbaminch';
+   document.getElementById('TourName').innerHTML='Arbaminch NechSar Park';
+}
+function changeGond(){
+    let attraction2 = document.getElementById('image');
+    attraction2.src = 'image/Gonder.jpg';
+    document.getElementById('TourCity').innerHTML='Gonder';
+    document.getElementById('TourName').innerHTML='Fasil Ghebbi';
+
+}
+function changeLal(){
+    let attraction3 = document.getElementById('image');  
+    attraction3.src = 'image/Lalibela.jpg';
+    document.getElementById('TourCity').innerHTML='Lalibela';
+    document.getElementById('TourName').innerHTML='Lalibela church';
+}
